478f948d1009e475f93067ce7ede09d96d25c76f
Prosenjit Manna <prosenjit@itobuz.com>
2025-03-06 23:53:50 +0530

    config updated for paralal test case

--- .mocharc.js
+++ .mocharc.js
@@ -1,3 +1,10 @@
 module.exports = {
-    timeout: 200000  // define a 20 seconds timeout for all tests
+    timeout: 200000,  // define a 20 seconds timeout for all tests
+    spec: ['src/**/*.test.ts'],  // updated to look for TypeScript spec files
+    reporter: 'spec',
+    recursive: true,
+    require: ['ts-node/register'],  // require ts-node/register to compile TypeScript files
+    extension: ['ts'],  // file extensions to look for
+    parallel: true,  // enable parallel execution
+    jobs: 2  // number of parallel jobs
 };
